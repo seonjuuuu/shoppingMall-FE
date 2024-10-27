99640966a33d94d6412342c39936c20bf960f08c
MoonSsunny <moonsun116@naver.com>
2024-10-27 20:49:30 +0900

    [feat] 수정

--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
-import ProductCard from "./components/ProductCard";
-import { Row, Col, Container } from "react-bootstrap";
-import { useSearchParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getProductList } from "../../features/product/productSlice";
+import React, { useEffect } from 'react';
+import ProductCard from './components/ProductCard';
+import { Row, Col, Container } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProductList } from '../../features/product/productSlice';
 
 const LandingPage = () => {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.product.productList);
   const [query] = useSearchParams();
-  const name = query.get("name");
+  const name = query.get('name');
   useEffect(() => {
     dispatch(
       getProductList({
@@ -30,10 +30,10 @@ const LandingPage = () => {
           ))
         ) : (
           <div className="text-align-center empty-bag">
-            {name === "" ? (
+            {name === '' ? (
               <h2>등록된 상품이 없습니다!</h2>
             ) : (
-              <h2>{name}과 일치한 상품이 없습니다!`</h2>
+              <h2>{name}과 일치한 상품이 없습니다!</h2>
             )}
           </div>
         )}
