d99001c86a59c7b3123db483a9451b52872fe8f3
MoonSsunny <moonsun116@naver.com>
2024-10-27 15:14:25 +0900

    [feat] overlay 추가

--- src/common/component/Navbar.js
+++ src/common/component/Navbar.js
@@ -18,7 +18,23 @@ const Navbar = ({ user }) => {
   const { cartItemCount } = useSelector((state) => state.cart);
   const isMobile = window.navigator.userAgent.indexOf('Mobile') !== -1;
   const [showSearchBox, setShowSearchBox] = useState(false);
-  const [width, setWidth] = useState(0);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
+  const onCheckEnter = (event) => {
+    if (event.key === 'Enter') {
+      navigate(event.target.value === '' ? '/' : `?name=${event.target.value}`);
+    }
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    closeMenu();
+  };
+
   const menuList = [
     '여성',
     'Divided',
@@ -29,25 +45,88 @@ const Navbar = ({ user }) => {
     'Sale',
     '지속가능성',
   ];
-  const navigate = useNavigate();
-
-  const onCheckEnter = (event) => {
-    if (event.key === 'Enter') {
-      navigate(event.target.value === '' ? '/' : `?name=${event.target.value}`);
-    }
-  };
-
-  const handleLogout = () => {
-    dispatch(logout());
-  };
 
   return (
     <div>
+      <div
+        className={`${styles.overlay} ${menuOpen ? styles.active : ''}`}
+        onClick={closeMenu}
+      ></div>
+
+      <div
+        className={styles.sideMenu}
+        style={{ width: menuOpen ? '250px' : '0' }}
+      >
+        <button className={styles.closeBtn} onClick={closeMenu}>
+          &times;
+        </button>
+        <div className={styles.sideMenuList}>
+          {menuList.map((menu, index) => (
+            <button key={index}>{menu}</button>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.navHeader}>
+        <div
+          className={`${styles.burgerMenu} ${styles.hide}`}
+          onClick={toggleMenu}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        <div className={styles.displayFlex}>
+          {user ? (
+            <div onClick={handleLogout} className={styles.navIcon}>
+              <FontAwesomeIcon icon={faUser} />
+              {!isMobile && <span className={styles.navName}>로그아웃</span>}
+            </div>
+          ) : (
+            <div onClick={() => navigate('/login')} className={styles.navIcon}>
+              <FontAwesomeIcon icon={faUser} />
+              {!isMobile && <span className={styles.navName}>로그인</span>}
+            </div>
+          )}
+          <div onClick={() => navigate('/cart')} className={styles.navIcon}>
+            <FontAwesomeIcon icon={faShoppingBag} />
+            {!isMobile && (
+              <span
+                className={styles.navName}
+              >{`쇼핑백(${cartItemCount || 0})`}</span>
+            )}
+          </div>
+          <div
+            onClick={() => navigate('/account/purchase')}
+            className={styles.navIcon}
+          >
+            <FontAwesomeIcon icon={faBox} />
+            {!isMobile && <span className={styles.navName}>내 주문</span>}
+          </div>
+          {isMobile && (
+            <div
+              className={styles.navIcon}
+              onClick={() => setShowSearchBox(true)}
+            >
+              <FontAwesomeIcon icon={faSearch} />
+            </div>
+          )}
+          {user && user.level === 'admin' && (
+            <div className={styles.navIcon}>
+              <FontAwesomeIcon icon={faUserShield} />
+              <Link to="/admin/product?page=1" className={styles.linkArea}>
+                {!isMobile && (
+                  <span className={styles.navName}>관리자 페이지</span>
+                )}
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+
       {showSearchBox && (
         <div
           className={`${styles.displaySpaceBetween} ${styles.mobileSearchBox}`}
         >
-          <div className={`${styles.search} ${styles.displaySpaceBetween} `}>
+          <div className={`${styles.search} ${styles.displaySpaceBetween}`}>
             <div>
               <FontAwesomeIcon className={styles.searchIcon} icon={faSearch} />
               <input
@@ -65,72 +144,7 @@ const Navbar = ({ user }) => {
           </div>
         </div>
       )}
-      <div className={styles.sideMenu} style={{ width }}>
-        <button className={styles.closeBtn} onClick={() => setWidth(0)}>
-          &times;
-        </button>
-        <div className={styles.sideMenuList} id="menu-list">
-          {menuList.map((menu, index) => (
-            <button key={index}>{menu}</button>
-          ))}
-        </div>
-      </div>
-      <div className={styles.navHeader}>
-        <div className={`${styles.burgerMenu} ${styles.hide}`}>
-          <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
-        </div>
-        <div>
-          <div className={styles.displayFlex}>
-            {user ? (
-              <div onClick={handleLogout} className={styles.navIcon}>
-                <FontAwesomeIcon icon={faUser} />
-                {!isMobile && <span>로그아웃</span>}
-              </div>
-            ) : (
-              <div
-                onClick={() => navigate('/login')}
-                className={styles.navIcon}
-              >
-                <FontAwesomeIcon icon={faUser} />
-                {!isMobile && <span>로그인</span>}
-              </div>
-            )}
-            <div onClick={() => navigate('/cart')} className={styles.navIcon}>
-              <FontAwesomeIcon icon={faShoppingBag} />
-              {!isMobile && (
-                <span
-                  className={styles.navName}
-                >{`쇼핑백(${cartItemCount || 0})`}</span>
-              )}
-            </div>
-            <div
-              onClick={() => navigate('/account/purchase')}
-              className={styles.navIcon}
-            >
-              <FontAwesomeIcon icon={faBox} />
-              {!isMobile && <span className={styles.navName}>내 주문</span>}
-            </div>
-            {isMobile && (
-              <div
-                className={styles.navIcon}
-                onClick={() => setShowSearchBox(true)}
-              >
-                <FontAwesomeIcon icon={faSearch} />
-              </div>
-            )}
-            {user && user.level === 'admin' && (
-              <div className={styles.navIcon}>
-                <FontAwesomeIcon icon={faUserShield} />
-                <Link to="/admin/product?page=1" className={styles.linkArea}>
-                  {!isMobile && (
-                    <span className={styles.navName}>관리자 페이지</span>
-                  )}
-                </Link>
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
+
       <div className={styles.navLogo}>
         <Link to="/">
           <img width={200} src="/image/logo.png" alt="logo.png" />
