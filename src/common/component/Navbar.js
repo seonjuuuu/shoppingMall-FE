f5e66dcea909dabcf646497881c44963309702c3
MoonSsunny <moonsun116@naver.com>
2024-10-27 15:03:27 +0900

    [feat] nav 정리

--- src/common/component/Navbar.js
+++ src/common/component/Navbar.js
@@ -6,17 +6,19 @@ import {
   faBox,
   faSearch,
   faShoppingBag,
+  faUserShield,
 } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../features/user/userSlice';
+import styles from './Navbar.module.scss';
 
 const Navbar = ({ user }) => {
   const dispatch = useDispatch();
   const { cartItemCount } = useSelector((state) => state.cart);
   const isMobile = window.navigator.userAgent.indexOf('Mobile') !== -1;
   const [showSearchBox, setShowSearchBox] = useState(false);
+  const [width, setWidth] = useState(0);
   const menuList = [
     '여성',
     'Divided',
@@ -27,26 +29,27 @@ const Navbar = ({ user }) => {
     'Sale',
     '지속가능성',
   ];
-  let [width, setWidth] = useState(0);
-  let navigate = useNavigate();
+  const navigate = useNavigate();
+
   const onCheckEnter = (event) => {
     if (event.key === 'Enter') {
-      if (event.target.value === '') {
-        return navigate('/');
-      }
-      navigate(`?name=${event.target.value}`);
+      navigate(event.target.value === '' ? '/' : `?name=${event.target.value}`);
     }
   };
+
   const handleLogout = () => {
     dispatch(logout());
   };
+
   return (
     <div>
       {showSearchBox && (
-        <div className="display-space-between mobile-search-box w-100">
-          <div className="search display-space-between w-100">
+        <div
+          className={`${styles.displaySpaceBetween} ${styles.mobileSearchBox}`}
+        >
+          <div className={`${styles.search} ${styles.displaySpaceBetween} `}>
             <div>
-              <FontAwesomeIcon className="search-icon" icon={faSearch} />
+              <FontAwesomeIcon className={styles.searchIcon} icon={faSearch} />
               <input
                 type="text"
                 placeholder="제품검색"
@@ -54,7 +57,7 @@ const Navbar = ({ user }) => {
               />
             </div>
             <button
-              className="closebtn"
+              className={styles.closeBtn}
               onClick={() => setShowSearchBox(false)}
             >
               &times;
@@ -62,81 +65,87 @@ const Navbar = ({ user }) => {
           </div>
         </div>
       )}
-      <div className="side-menu" style={{ width: width }}>
-        <button className="closebtn" onClick={() => setWidth(0)}>
+      <div className={styles.sideMenu} style={{ width }}>
+        <button className={styles.closeBtn} onClick={() => setWidth(0)}>
           &times;
         </button>
-
-        <div className="side-menu-list" id="menu-list">
+        <div className={styles.sideMenuList} id="menu-list">
           {menuList.map((menu, index) => (
             <button key={index}>{menu}</button>
           ))}
         </div>
       </div>
-      {user && user.level === 'admin' && (
-        <Link to="/admin/product?page=1" className="link-area">
-          Admin page
-        </Link>
-      )}
-      <div className="nav-header">
-        <div className="burger-menu hide">
+      <div className={styles.navHeader}>
+        <div className={`${styles.burgerMenu} ${styles.hide}`}>
           <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
         </div>
-
         <div>
-          <div className="display-flex">
+          <div className={styles.displayFlex}>
             {user ? (
-              <div onClick={handleLogout} className="nav-icon">
+              <div onClick={handleLogout} className={styles.navIcon}>
                 <FontAwesomeIcon icon={faUser} />
-                {!isMobile && (
-                  <span style={{ cursor: 'pointer' }}>로그아웃</span>
-                )}
+                {!isMobile && <span>로그아웃</span>}
               </div>
             ) : (
-              <div onClick={() => navigate('/login')} className="nav-icon">
+              <div
+                onClick={() => navigate('/login')}
+                className={styles.navIcon}
+              >
                 <FontAwesomeIcon icon={faUser} />
-                {!isMobile && <span style={{ cursor: 'pointer' }}>로그인</span>}
+                {!isMobile && <span>로그인</span>}
               </div>
             )}
-            <div onClick={() => navigate('/cart')} className="nav-icon">
+            <div onClick={() => navigate('/cart')} className={styles.navIcon}>
               <FontAwesomeIcon icon={faShoppingBag} />
               {!isMobile && (
-                <span style={{ cursor: 'pointer' }}>{`쇼핑백(${
-                  cartItemCount || 0
-                })`}</span>
+                <span
+                  className={styles.navName}
+                >{`쇼핑백(${cartItemCount || 0})`}</span>
               )}
             </div>
             <div
               onClick={() => navigate('/account/purchase')}
-              className="nav-icon"
+              className={styles.navIcon}
             >
               <FontAwesomeIcon icon={faBox} />
-              {!isMobile && <span style={{ cursor: 'pointer' }}>내 주문</span>}
+              {!isMobile && <span className={styles.navName}>내 주문</span>}
             </div>
             {isMobile && (
-              <div className="nav-icon" onClick={() => setShowSearchBox(true)}>
+              <div
+                className={styles.navIcon}
+                onClick={() => setShowSearchBox(true)}
+              >
                 <FontAwesomeIcon icon={faSearch} />
+              </div>
+            )}
+            {user && user.level === 'admin' && (
+              <div className={styles.navIcon}>
+                <FontAwesomeIcon icon={faUserShield} />
+                <Link to="/admin/product?page=1" className={styles.linkArea}>
+                  {!isMobile && (
+                    <span className={styles.navName}>관리자 페이지</span>
+                  )}
+                </Link>
               </div>
             )}
           </div>
         </div>
       </div>
-
-      <div className="nav-logo">
+      <div className={styles.navLogo}>
         <Link to="/">
           <img width={200} src="/image/logo.png" alt="logo.png" />
         </Link>
       </div>
-      <div className="nav-menu-area">
-        <ul className="menu">
+      <div className={styles.navMenuArea}>
+        <ul className={styles.menu}>
           {menuList.map((menu, index) => (
             <li key={index}>
               <a href="#">{menu}</a>
             </li>
           ))}
         </ul>
-        {!isMobile && ( // admin페이지에서 같은 search-box스타일을 쓰고있음 그래서 여기서 서치박스 안보이는것 처리를 해줌
-          <div className="search-box landing-search-box ">
+        {!isMobile && (
+          <div className={`${styles.searchBox} ${styles.landingSearchBox}`}>
             <FontAwesomeIcon icon={faSearch} />
             <input
               type="text"
