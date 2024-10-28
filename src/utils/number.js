ae04f9082c7ea6a8203ba9ae551e41eaadd600e7
MoonSsunny <moonsun116@naver.com>
2024-10-28 16:51:43 +0900

    [feat] 할인율 구하기

--- src/utils/number.js
+++ src/utils/number.js
@@ -1,28 +1,32 @@
 export const currencyFormat = (value) => {
   const number = value !== undefined ? value : 0;
-  return number.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+  return number.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
 };
 
 export const cc_expires_format = (string) => {
   return string
     .replace(
       /[^0-9]/g,
-      "" // To allow only numbers
+      '' // To allow only numbers
     )
     .replace(
       /^([2-9])$/g,
-      "0$1" // To handle 3 > 03
+      '0$1' // To handle 3 > 03
     )
     .replace(
       /^(1{1})([3-9]{1})$/g,
-      "0$1/$2" // 13 > 01/3
+      '0$1/$2' // 13 > 01/3
     )
     .replace(
       /^0{1,}/g,
-      "0" // To handle 00 > 0
+      '0' // To handle 00 > 0
     )
     .replace(
       /^([0-1]{1}[0-9]{1})([0-9]{1,2}).*/g,
-      "$1/$2" // To handle 113 > 11/3
+      '$1/$2' // To handle 113 > 11/3
     );
+};
+
+export const discountPercent = (price, discountPrice) => {
+  return ((price - discountPrice) / price) * 100;
 };
