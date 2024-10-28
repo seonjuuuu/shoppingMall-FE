653a356c106a44b6ca93116ed9a1edd98294fce9
MoonSsunny <moonsun116@naver.com>
2024-10-28 15:06:32 +0900

    [feat] search-box 수정

--- src/common/component/SearchBox.js
+++ src/common/component/SearchBox.js
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { useSearchParams } from "react-router-dom";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useSearchParams } from 'react-router-dom';
+import styles from './SearchBox.module.scss';
 
 const SearchBox = ({ searchQuery, setSearchQuery, placeholder, field }) => {
   const [query] = useSearchParams();
-  const [keyword, setKeyword] = useState(query.get(field) || "");
+  const [keyword, setKeyword] = useState(query.get(field) || '');
 
   const onCheckEnter = (event) => {
-    if (event.key === "Enter") {
+    if (event.key === 'Enter') {
       setSearchQuery({ ...searchQuery, page: 1, [field]: event.target.value });
     }
   };
   return (
-    <div className="search-box">
+    <div className={styles.searchBox}>
       <FontAwesomeIcon icon={faSearch} />
       <input
         type="text"
