984cb62fa29ce537dbdab4e1142cff0909417fa4
MoonSsunny <moonsun116@naver.com>
2024-10-29 19:44:41 +0900

    [feat] 코드정리

--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -9,77 +9,55 @@ import LoadingSpinner from '../../common/component/LoadingSpinner';
 
 const LandingPage = () => {
   const dispatch = useDispatch();
-
   const productList = useSelector((state) => state.product.productList);
   const loading = useSelector((state) => state.product.loading);
-
   const [query, setSearchParams] = useSearchParams();
-  const name = query.get('name');
-  useEffect(() => {
-    dispatch(
-      getProductList({
-        name,
-      })
-    );
-  }, [query]);
 
-  const moveAllProduct = () => {
-    setSearchParams({});
-  };
-
+  const name = query.get('name') || '';
   const filterList = productList.filter((item) => item.status === 'active');
 
-  return (
-    <>
-      {loading ? (
-        <LoadingSpinner />
-      ) : (
-        <>
-          {name && name?.length > 0 && filterList.length > 0 && (
-            <div className={styles.searchInfo}>
-              "<span className={styles.serachName}>{name}</span>" 에 대한
-              검색결과 {filterList.length}건
-            </div>
-          )}
-          <Container className={styles.landingPage}>
-            <Row>
-              {filterList.length > 0 ? (
-                filterList.map((item) => (
-                  <Col lg={3} md={6} sm={12} key={item._id}>
-                    <ProductCard item={item} />
-                  </Col>
-                ))
-              ) : (
-                <div className={styles.productText}>
-                  <img
-                    src="/image/no-data.png"
-                    alt="no-data"
-                    className={styles.noDataImage}
-                  />
-                  {name === '' || filterList.length === 0 ? (
-                    <div className={styles.noProduct}>
-                      등록된 상품이 없습니다!
-                    </div>
-                  ) : (
-                    <div className={styles.noProduct}>
-                      "{name}"와 일치한 상품이 없습니다!
-                    </div>
-                  )}
-                  {filterList.length !== 0 && (
-                    <button
-                      className={styles.allButton}
-                      onClick={moveAllProduct}
-                    >
-                      전체 상품 보기
-                    </button>
-                  )}
-                </div>
-              )}
-            </Row>
-          </Container>
-        </>
+  useEffect(() => {
+    dispatch(getProductList({ name }));
+  }, [name, dispatch]);
+
+  const moveAllProduct = () => setSearchParams({});
+
+  return loading ? (
+    <LoadingSpinner />
+  ) : (
+    <Container className={styles.landingPage}>
+      {name && filterList.length > 0 && (
+        <div className={styles.searchInfo}>
+          "<span className={styles.searchName}>{name}</span>" 에 대한 검색결과
+          {filterList.length}건
+        </div>
       )}
-    </>
+      <Row>
+        {filterList.length > 0 ? (
+          filterList.map((item) => (
+            <Col lg={3} md={6} sm={12} key={item._id}>
+              <ProductCard item={item} />
+            </Col>
+          ))
+        ) : (
+          <div className={styles.productText}>
+            <img
+              src="/image/no-data.png"
+              alt="no-data"
+              className={styles.noDataImage}
+            />
+            <div className={styles.noProduct}>
+              {name
+                ? `"${name}"와 일치한 상품이 없습니다!`
+                : '등록된 상품이 없습니다!'}
+            </div>
+            <button className={styles.allButton} onClick={moveAllProduct}>
+              전체 상품 보기
+            </button>
+          </div>
+        )}
+      </Row>
+    </Container>
   );
 };
 
