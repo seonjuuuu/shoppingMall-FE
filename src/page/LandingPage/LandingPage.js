b28d28708a6957f1f82b594295c533c6a66548ee
MoonSsunny <moonsun116@naver.com>
2024-10-29 19:31:27 +0900

    [feat] 코드정리

--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -30,51 +30,56 @@ const LandingPage = () => {
   const filterList = productList.filter((item) => item.status === 'active');
 
   return (
-    <>    
-    {loading ? <LoadingSpinner /> :
-    <>    
-    {name &&  name?.length > 0 && filterList.length > 0 && 
-      <div className={styles.searchInfo}>
-        "<span className={styles.serachName}>{name}</span>"
-        에 대한 검색결과 {filterList.length}건 
-      </div>
-    }
-    <Container className={styles.landingPage}>
-      <Row>
-        {filterList.length > 0 ? (
-          filterList
-            .map((item) => (
-              <Col lg={3} md={6} sm={12} key={item._id}>
-                <ProductCard item={item} />
-              </Col>
-            ))
-        ) : (
-          <div className={styles.productText}>
-            <img
-              src="/image/no-data.png"
-              alt="no-data"
-              className={styles.noDataImage}
-            />
-            {name === '' || filterList.length === 0 ? (
-              <div className={styles.noProduct}>등록된 상품이 없습니다!</div>
-            ) : (
-              <div className={styles.noProduct}>
-                "{name}"와 일치한 상품이 없습니다!
-              </div>
-            )}
-            {filterList.length !== 0 && 
-              <button className={styles.allButton} onClick={moveAllProduct}>
-                전체 상품 보기
-              </button>
-            }
-          </div>
-        )}
-      </Row>
-    </Container>
+    <>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          {name && name?.length > 0 && filterList.length > 0 && (
+            <div className={styles.searchInfo}>
+              "<span className={styles.serachName}>{name}</span>" 에 대한
+              검색결과 {filterList.length}건
+            </div>
+          )}
+          <Container className={styles.landingPage}>
+            <Row>
+              {filterList.length > 0 ? (
+                filterList.map((item) => (
+                  <Col lg={3} md={6} sm={12} key={item._id}>
+                    <ProductCard item={item} />
+                  </Col>
+                ))
+              ) : (
+                <div className={styles.productText}>
+                  <img
+                    src="/image/no-data.png"
+                    alt="no-data"
+                    className={styles.noDataImage}
+                  />
+                  {name === '' || filterList.length === 0 ? (
+                    <div className={styles.noProduct}>
+                      등록된 상품이 없습니다!
+                    </div>
+                  ) : (
+                    <div className={styles.noProduct}>
+                      "{name}"와 일치한 상품이 없습니다!
+                    </div>
+                  )}
+                  {filterList.length !== 0 && (
+                    <button
+                      className={styles.allButton}
+                      onClick={moveAllProduct}
+                    >
+                      전체 상품 보기
+                    </button>
+                  )}
+                </div>
+              )}
+            </Row>
+          </Container>
+        </>
+      )}
     </>
-    }
-    </>
-
   );
 };
 
