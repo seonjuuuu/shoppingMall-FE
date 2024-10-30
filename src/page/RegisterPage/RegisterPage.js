86e023dc2635a82f4cc94a50484553a8ede9f505
MoonSsunny <moonsun116@naver.com>
2024-10-30 17:09:00 +0900

    [feat] 안쓰는 코드 정리

--- src/page/RegisterPage/RegisterPage.js
+++ src/page/RegisterPage/RegisterPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Container, Form, Button, Alert } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
