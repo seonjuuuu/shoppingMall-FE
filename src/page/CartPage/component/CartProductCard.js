2a07f24b99bbf6f96c0ffa8e86675546857567fb
MoonSsunny <moonsun116@naver.com>
2024-11-03 22:29:16 +0900

    [feat] 수정

--- src/page/CartPage/component/CartProductCard.js
+++ src/page/CartPage/component/CartProductCard.js
@@ -31,60 +31,67 @@ const CartProductCard = ({ item }) => {
     <div
       className={`${styles.productCardCart} ${isDeleting ? styles.deleting : ''}`}
     >
-      <div className={styles.productCard}>
-        <Link
-          to={`/product/${item.productId._id}`}
-          className={styles.imageContainer}
-        >
-          <img src={item.productId.image} width={112} alt="product" />
-        </Link>
-        <div className={styles.detailsContainer}>
-          <div className={`${styles.flex} ${styles.spaceBetween}`}>
-            <h3 className={styles.productName}>{item.productId.name}</h3>
-            <button className={styles.trashButton}>
-              <FontAwesomeIcon
-                icon={faTimes}
-                width={24}
-                onClick={() => deleteCart(item._id)}
-              />
-            </button>
-          </div>
-
-          <div className={styles.productInfo}>
-            <strong className={styles.productPrice}>
-              ₩ {currencyFormat(item.productId.discountPrice)}
-            </strong>
-            <div>사이즈: {item.size}</div>
-            <div className={styles.productNum}>
-              수량:
-              <select
-                onChange={(event) =>
-                  handleQtyChange(item._id, event.target.value)
-                }
-                required
-                defaultValue={item.qty}
-                className={styles.qtyDropdown}
-              >
-                {[...Array(10)].map((_, index) => (
-                  <option key={`${item._id}-${index}`} value={index + 1}>
-                    {index + 1}
-                  </option>
-                ))}
-              </select>
+      {isDeleting ? (
+        <div className={styles.loadingContainer}>
+          <BeatLoader size={10} />
+          <span>삭제 중입니다...</span>
+        </div>
+      ) : (
+        <div className={styles.productCard}>
+          <Link
+            to={`/product/${item.productId._id}`}
+            className={styles.imageContainer}
+          >
+            <img src={item.productId.image} width={112} alt="product" />
+          </Link>
+          <div className={styles.detailsContainer}>
+            <div className={`${styles.flex} ${styles.spaceBetween}`}>
+              <h3 className={styles.productName}>{item.productId.name}</h3>
+              <button className={styles.trashButton}>
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  width={24}
+                  onClick={() => deleteCart(item._id)}
+                />
+              </button>
             </div>
-            <div className={styles.productTotal}>
-              Total:
-              {isLoading ? (
-                <div className={styles.loading}>
-                  <BeatLoader size={10} />
-                </div>
-              ) : (
-                ` ₩ ${currencyFormat(item.productId.discountPrice * item.qty)}`
-              )}
+
+            <div className={styles.productInfo}>
+              <strong className={styles.productPrice}>
+                ₩ {currencyFormat(item.productId.discountPrice)}
+              </strong>
+              <div>사이즈: {item.size}</div>
+              <div className={styles.productNum}>
+                수량:
+                <select
+                  onChange={(event) =>
+                    handleQtyChange(item._id, event.target.value)
+                  }
+                  required
+                  defaultValue={item.qty}
+                  className={styles.qtyDropdown}
+                >
+                  {[...Array(10)].map((_, index) => (
+                    <option key={`${item._id}-${index}`} value={index + 1}>
+                      {index + 1}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className={styles.productTotal}>
+                Total:
+                {isLoading ? (
+                  <div className={styles.loading}>
+                    <BeatLoader size={10} />
+                  </div>
+                ) : (
+                  ` ₩ ${currencyFormat(item.productId.discountPrice * item.qty)}`
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
